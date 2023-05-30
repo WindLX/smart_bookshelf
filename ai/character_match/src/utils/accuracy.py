8305525619b9e236e957d9860eefb28222b18373
wind <1418043337@qq.com>
2023-05-30 21:47:59 +0800

    [update] comment for acc

--- ai/character_match/src/utils/accuracy.py
+++ ai/character_match/src/utils/accuracy.py
@@ -1,4 +1,13 @@
 def accuracy(labels, predicts) -> float:
+    """计算预测准确度
+
+    Args:
+        labels (_type_): 标签
+        predicts (_type_): 预测值
+
+    Returns:
+        float: 准确率
+    """
     prop = 0.5
     lens = len(labels)
     right = 0
