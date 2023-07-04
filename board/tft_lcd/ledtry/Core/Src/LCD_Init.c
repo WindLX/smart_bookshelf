79baaf634008c06a876cdc6aca12960999b56fc6
Lue <luecpp@gmail.cn>
2023-07-04 19:22:47 +0800

    [add]oled function

---
+++ board/tft_lcd/ledtry/Core/Src/LCD_Init.c
@@ -0,0 +1,193 @@
+#include "main.h"
+#include "LCD_Init.h"
+#include "spi.h"
+
+// void delay(int t)
+// {
+// 	while(t--);
+// }
+
+// 定义发送数据的函数
+void LCD_WR_DATA8(uint8_t data)
+{
+    HAL_GPIO_WritePin(LCD_DC_GPIO_Port, LCD_DC_Pin, GPIO_PIN_SET); // 设置为数据模式
+    HAL_SPI_Transmit(&hspi1, &data, 1, HAL_MAX_DELAY); // 发送数据
+    // HAL_GPIO_WritePin(LCD_CS_GPIO_Port, LCD_DC_Pin, GPIO_PIN_RESET); 
+  	// while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);//检查接收标志位
+	// SPI_I2S_SendData(SPI1,dat);
+	// delay(1);
+    // HAL_GPIO_WritePin(LCD_CS_GPIO_Port, LCD_DC_Pin, GPIO_PIN_SET); 
+}
+
+/******************************************************************************
+      函数说明：LCD写入数据
+      入口数据：dat 写入的数据
+      返回值：  无
+******************************************************************************/
+void LCD_WR_DATA(uint16_t dat)
+{
+	LCD_WR_DATA8(dat>>8);
+	LCD_WR_DATA8(dat);
+}
+
+// 定义发送命令的函数
+void LCD_WR_REG(uint8_t command)
+{
+    HAL_GPIO_WritePin(LCD_DC_GPIO_Port, LCD_DC_Pin, GPIO_PIN_RESET); // 设置为命令模式
+    HAL_SPI_Transmit(&hspi1, &command, 1, HAL_MAX_DELAY); // 发送命令
+    // HAL_GPIO_WritePin(LCD_DC_GPIO_Port, LCD_DC_Pin, GPIO_PIN_RESET); 
+	// LCD_WR_DATA8(dat);
+    // HAL_GPIO_WritePin(LCD_DC_GPIO_Port, LCD_DC_Pin, GPIO_PIN_SET); 
+}
+
+/******************************************************************************
+      函数说明：设置起始和结束地址
+      入口数据：x1,x2 设置列的起始和结束地址
+                y1,y2 设置行的起始和结束地址
+      返回值：  无
+******************************************************************************/
+void LCD_Address_Set(uint16_t x1,uint16_t y1,uint16_t x2,uint16_t y2)
+{
+	if(USE_HORIZONTAL==0)
+	{
+		LCD_WR_REG(0x2a);//列地址设置
+		LCD_WR_DATA(x1+2);
+		LCD_WR_DATA(x2+2);
+		LCD_WR_REG(0x2b);//行地址设置
+		LCD_WR_DATA(y1+1);
+		LCD_WR_DATA(y2+1);
+		LCD_WR_REG(0x2c);//储存器写
+	}
+	else if(USE_HORIZONTAL==1)
+	{
+		LCD_WR_REG(0x2a);//列地址设置
+		LCD_WR_DATA(x1+2);
+		LCD_WR_DATA(x2+2);
+		LCD_WR_REG(0x2b);//行地址设置
+		LCD_WR_DATA(y1+1);
+		LCD_WR_DATA(y2+1);
+		LCD_WR_REG(0x2c);//储存器写
+	}
+	else if(USE_HORIZONTAL==2)
+	{
+		LCD_WR_REG(0x2a);//列地址设置
+		LCD_WR_DATA(x1+1);
+		LCD_WR_DATA(x2+1);
+		LCD_WR_REG(0x2b);//行地址设置
+		LCD_WR_DATA(y1+2);
+		LCD_WR_DATA(y2+2);
+		LCD_WR_REG(0x2c);//储存器写
+	}
+	else
+	{
+		LCD_WR_REG(0x2a);//列地址设置
+		LCD_WR_DATA(x1+1);
+		LCD_WR_DATA(x2+1);
+		LCD_WR_REG(0x2b);//行地址设置
+		LCD_WR_DATA(y1+2);
+		LCD_WR_DATA(y2+2);
+		LCD_WR_REG(0x2c);//储存器写
+	}
+}
+
+void LCD_Init(void){
+    // 复位
+    HAL_GPIO_WritePin(LCD_BL_GPIO_Port, LCD_BL_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(LCD_RST_GPIO_Port, LCD_RST_Pin, GPIO_PIN_RESET);
+    HAL_Delay(100);
+    HAL_GPIO_WritePin(LCD_RST_GPIO_Port, LCD_RST_Pin, GPIO_PIN_SET);
+    HAL_Delay(100);
+    
+    //************* Start Initial Sequence **********//
+	LCD_WR_REG(0x11); //Sleep out 
+	HAL_Delay(120);              //Delay 120ms 
+	//------------------------------------ST7735S Frame Rate-----------------------------------------// 
+	LCD_WR_REG(0xB1); 
+	LCD_WR_DATA8(0x05); 
+	LCD_WR_DATA8(0x3C); 
+	LCD_WR_DATA8(0x3C); 
+	LCD_WR_REG(0xB2); 
+	LCD_WR_DATA8(0x05);
+	LCD_WR_DATA8(0x3C); 
+	LCD_WR_DATA8(0x3C); 
+	LCD_WR_REG(0xB3); 
+	LCD_WR_DATA8(0x05); 
+	LCD_WR_DATA8(0x3C); 
+	LCD_WR_DATA8(0x3C); 
+	LCD_WR_DATA8(0x05); 
+	LCD_WR_DATA8(0x3C); 
+	LCD_WR_DATA8(0x3C); 
+	//------------------------------------End ST7735S Frame Rate---------------------------------// 
+	LCD_WR_REG(0xB4); //Dot inversion 
+	LCD_WR_DATA8(0x03); 
+	//------------------------------------ST7735S Power Sequence---------------------------------// 
+	LCD_WR_REG(0xC0); 
+	LCD_WR_DATA8(0x28); 
+	LCD_WR_DATA8(0x08); 
+	LCD_WR_DATA8(0x04); 
+	LCD_WR_REG(0xC1); 
+	LCD_WR_DATA8(0XC0); 
+	LCD_WR_REG(0xC2); 
+	LCD_WR_DATA8(0x0D); 
+	LCD_WR_DATA8(0x00); 
+	LCD_WR_REG(0xC3); 
+	LCD_WR_DATA8(0x8D); 
+	LCD_WR_DATA8(0x2A); 
+	LCD_WR_REG(0xC4); 
+	LCD_WR_DATA8(0x8D); 
+	LCD_WR_DATA8(0xEE); 
+	//---------------------------------End ST7735S Power Sequence-------------------------------------// 
+	LCD_WR_REG(0xC5); //VCOM 
+	LCD_WR_DATA8(0x1A); 
+	LCD_WR_REG(0x36); //MX, MY, RGB mode 
+	if(USE_HORIZONTAL==0)LCD_WR_DATA8(0x00);
+	else if(USE_HORIZONTAL==1)LCD_WR_DATA8(0xC0);
+	else if(USE_HORIZONTAL==2)LCD_WR_DATA8(0x70);
+	else LCD_WR_DATA8(0xA0); 
+	//------------------------------------ST7735S Gamma Sequence---------------------------------// 
+	LCD_WR_REG(0xE0); 
+	LCD_WR_DATA8(0x04); 
+	LCD_WR_DATA8(0x22); 
+	LCD_WR_DATA8(0x07); 
+	LCD_WR_DATA8(0x0A); 
+	LCD_WR_DATA8(0x2E); 
+	LCD_WR_DATA8(0x30); 
+	LCD_WR_DATA8(0x25); 
+	LCD_WR_DATA8(0x2A); 
+	LCD_WR_DATA8(0x28); 
+	LCD_WR_DATA8(0x26); 
+	LCD_WR_DATA8(0x2E); 
+	LCD_WR_DATA8(0x3A); 
+	LCD_WR_DATA8(0x00); 
+	LCD_WR_DATA8(0x01); 
+	LCD_WR_DATA8(0x03); 
+	LCD_WR_DATA8(0x13); 
+	LCD_WR_REG(0xE1); 
+	LCD_WR_DATA8(0x04); 
+	LCD_WR_DATA8(0x16); 
+	LCD_WR_DATA8(0x06); 
+	LCD_WR_DATA8(0x0D); 
+	LCD_WR_DATA8(0x2D); 
+	LCD_WR_DATA8(0x26); 
+	LCD_WR_DATA8(0x23); 
+	LCD_WR_DATA8(0x27); 
+	LCD_WR_DATA8(0x27); 
+	LCD_WR_DATA8(0x25); 
+	LCD_WR_DATA8(0x2D); 
+	LCD_WR_DATA8(0x3B); 
+	LCD_WR_DATA8(0x00); 
+	LCD_WR_DATA8(0x01); 
+	LCD_WR_DATA8(0x04); 
+	LCD_WR_DATA8(0x13); 
+	//------------------------------------End ST7735S Gamma Sequence-----------------------------// 
+	LCD_WR_REG(0x3A); //65k mode 
+	LCD_WR_DATA8(0x05); 
+	LCD_WR_REG(0x29); //Display on 
+
+	LCD_Fill(0,0,LCD_W,LCD_H,WHITE);
+    
+	// 打开背光
+    HAL_GPIO_WritePin(LCD_BL_GPIO_Port, LCD_BL_Pin, GPIO_PIN_SET);
+    HAL_Delay(100);
+
+}
